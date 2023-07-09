be9d100984a3d396445f9a3f7eb3e2ec508800de
codechefsong <133816663+codechefsong@users.noreply.github.com>
2023-07-09 19:28:55 -0400

    add my bags grid

--- packages/nextjs/components/board/Board.tsx
+++ packages/nextjs/components/board/Board.tsx
@@ -2,6 +2,21 @@ import { useState, useRef } from "react";
 
 import { useDrag, useDrop } from 'react-dnd';
 
+const myItems = [
+  {
+    id: `26`,
+    index: 26,
+    type: "mybags",
+    content: "TTT",
+  },
+  {
+    id: `27`,
+    index: 27,
+    type: "mybags",
+    content: "AAA",
+  }
+]
+
 const generateGridData = () => {
   const data = [];
   for (let row = 0; row < 5; row++) {
@@ -9,6 +24,7 @@ const generateGridData = () => {
       data.push({
         id: `cell-${row}-${col}`,
         index: data.length,
+        type: "base",
         content: data.length + 1,
       });
     }
@@ -16,10 +32,17 @@ const generateGridData = () => {
   return data;
 };
 
-const Cell = ({ id, content, index, gridData, setGridData }) => {
+const Cell = ({ id, content, type, index, gridData, setGridData, bagData, setBagData }) => {
   const handleDrop = (item, index) => {
     // Handle the drop logic here
     let newGrid = [...gridData];
+    console.log(item, index)
+
+    if(item.type === "mybags"){
+      newGrid[index].content = item.content;
+      const newBag = bagData
+      newBag.shift();
+    }
     const oldContent = gridData[index].content;
     newGrid[index].content = item.content;
     newGrid[item.index].content = oldContent;
@@ -28,7 +51,7 @@ const Cell = ({ id, content, index, gridData, setGridData }) => {
 
   const [{ isDragging }, drag] = useDrag(() => ({
     type: 'CELL',
-    item: { id, index, content },
+    item: { id, index, type, content },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
@@ -60,14 +83,27 @@ const Cell = ({ id, content, index, gridData, setGridData }) => {
 
 export const BoardMain = () => {
   const [gridData, setGridData] = useState(generateGridData());
+  const [bagData, setBagData] = useState(myItems);
 
   return (
-    <div style={{ width: "500px"}}>
-      <h1 className="mt-4 text-4xl">House</h1>
-      <div className="flex flex-wrap">
-        {gridData.map((item, index) => (
-          <Cell key={item.id} id={item.id} content={item.content} index={index} gridData={gridData} setGridData={setGridData} />
-        ))}
+    <div>
+      <div className="flex">
+        <div>
+          <h2 className="mt-4 text-3xl">House</h2>
+          <div className="flex flex-wrap" style={{ width: "500px"}}>
+            {gridData.map((item, index) => (
+              <Cell key={item.id} id={item.id} content={item.content} type={item.type} index={index} gridData={gridData} setGridData={setGridData} bagData={bagData} setBagData={setBagData} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2 className="mt-4 text-3xl">My Bag</h2>
+          <div className="flex flex-wrap" style={{ width: "500px"}}>
+            {bagData.map((item, index) => (
+              <Cell key={item.id} id={item.id} content={item.content} type={item.type} index={index} gridData={gridData} setGridData={setGridData} bagData={bagData} setBagData={setBagData} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
